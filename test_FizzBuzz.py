38d38fde4c6fdc7749fac6000f6faa07b5b7cb3a
Matthew Hotchkiss <hotchkma@oregonstate.edu>
2021-05-31 11:46:03 -0700

    Wrote second test, checks Buzz functionality for convert(n) when n%5==0

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -6,4 +6,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(FizzBuzz.convert(3),"Fizz")
         self.assertEqual(FizzBuzz.convert(9),"Fizz")
         self.assertEqual(FizzBuzz.convert(81),"Fizz")
-
+    def test_buzz(self):
+        self.assertEqual(FizzBuzz.convert(5),"Buzz")
+        self.assertEqual(FizzBuzz.convert(20),"Buzz")
+        self.assertEqual(FizzBuzz.convert(65),"Buzz")
