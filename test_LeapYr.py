51b52a9ce769b375bc7f852736d8ea35eb4511c6
Matthew Hotchkiss <hotchkma@oregonstate.edu>
2021-05-31 12:30:20 -0700

    Wrote 2nd test for n%4=0 but n%100!=0

--- test_LeapYr.py
+++ test_LeapYr.py
@@ -6,3 +6,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(LeapYr.go(3),0)
         self.assertEqual(LeapYr.go(43),0)
         self.assertEqual(LeapYr.go(2021),0)
+    def test_4not100(self):
+        self.assertEqual(LeapYr.go(16),1)
+        self.assertEqual(LeapYr.go(44),1)
+        self.assertEqual(LeapYr.go(2020),1)
+
